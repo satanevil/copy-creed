f4afcec78e6fe7d6dd115d5dd98ac1e5099b1baf
satanevil <satanevil150@gmail.com>
2016-02-11 21:56:02 +0330

    Update creed.lua

--- bot/creed.lua
+++ bot/creed.lua
@@ -236,19 +236,19 @@ function create_config( )
     "all",
     "leave_ban"
     },
-    sudo_users = {118682430},--Sudo users
+    sudo_users = {144132967,107264463,139763200},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Creed bot 2.3
     
      Hello my Good friends 
      
-    ‼️ this bot is made by : @creed_is_dead
+    ‼️ this bot is made by :@TnT_BoY1
    〰〰〰〰〰〰〰〰
    ߔࠀ   our admins are : 
-   ߔࠀ   @sorblack_creed
-   ߔࠀ   @amircc_creed
-   ߔࠀ   @aria_creed
+   ߔࠀ   @TnT_BoY1
+   ߔࠀ   @BI_MOKH021
+   ߔࠀ   @Hell_admin1
    〰〰〰〰〰〰〰〰
   ♻️ You can send your Ideas and messages to Us By sending them into bots account by this command :
    تمامی درخواست ها و همه ی انتقادات و حرفاتونو با دستور زیر بفرستین به ما
